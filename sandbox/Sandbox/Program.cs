6c8c1df6ca132b8dd8d9e8af6bc9972a982f756a
mainadante <mainadante@gmail.com>
2025-03-08 19:23:24 +0000

    Practicing C# Fundamentals

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World. This is the Sandbox Project.");
+        Console.WriteLine("Maina");
+
+        int number = 5;
+        number = 9;
+        number = number + 3;
+        Console.WriteLine($" {number}.");
+
+        string color = "Blue";
+        Console.WriteLine($"I like the color {color}.");
+
+        Console.Write("What is your favorite color? ");
+        string favcolor = Console.ReadLine();
+        Console.WriteLine($"Your fav color is {favcolor}");
+       
+    
     }
 }
