ecea66e4dccaaeda3adcd382fba186071c4c63b0
mainadante <mainadante@gmail.com>
2025-03-14 15:38:11 +0000

    Week02

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Job
+
+        {
+            public string _company;
+            public string _jobTitle;
+            public double _startYear;
+            public double _endYear;
+
+            public void DisplayJobDetails()
+            {
+                Console.WriteLine($"{_jobTitle} {_company} {_startYear}-{_endYear}")
+
+            }
+        }
